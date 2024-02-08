daa99fe759d99cb1b6c2126c666a113fa0b52d98
Yuri Martins <yurigamer16@gmail.com>
2024-02-08 20:55:21 -0300

    fea app 2

---
+++ app2/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <button style={{
+        margin: "10px",
+        padding:"10px",
+        textAlign:"center",
+        backgroundColor:"cyan",
+        border: "none"
+      }}>Second Button</button>
+    </div>
+  );
+}
+
+export default App;
